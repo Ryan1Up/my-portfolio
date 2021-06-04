063de828e1db7055aee665291b970e7f4ee43983
Ryan Anderson <anderson94@mail.fresnostate.edu>
2021-06-04 10:45:03 +0000

    Modify UI to look pleasant, add socials

--- portfolio/src/main/java/com/google/sps/ServerMain.java
+++ portfolio/src/main/java/com/google/sps/ServerMain.java
@@ -15,30 +15,28 @@ import org.eclipse.jetty.webapp.WebInfConfiguration;
  */
 public class ServerMain {
 
-  public static void main(String[] args) throws Exception {
+  public static void main(final String[] args) throws Exception {
 
-    // Create a server that listens on port 8080.
-    Server server = new Server(8080);
-    WebAppContext webAppContext = new WebAppContext();
-    server.setHandler(webAppContext);
+        // Create a server that listens on port 8080.
+        final Server server = new Server(8080);
+        final WebAppContext webAppContext = new WebAppContext();
+        server.setHandler(webAppContext);
 
-    // Load static content from inside the jar file.
-    URL webAppDir = ServerMain.class.getClassLoader().getResource("META-INF/resources");
-    webAppContext.setResourceBase(webAppDir.toURI().toString());
+        // Load static content from inside the jar file.
+        final URL webAppDir = ServerMain.class.getClassLoader().getResource("META-INF/resources");
+        webAppContext.setResourceBase(webAppDir.toURI().toString());
 
-    // Enable annotations so the server sees classes annotated with @WebServlet.
-    webAppContext.setConfigurations(
-        new Configuration[] {
-          new AnnotationConfiguration(), new WebInfConfiguration(),
-        });
+        // Enable annotations so the server sees classes annotated with @WebServlet.
+        webAppContext
+                .setConfigurations(new Configuration[] { new AnnotationConfiguration(), new WebInfConfiguration(), });
 
-    // Look for annotations in the classes directory (dev server) and in the jar file (live server)
-    webAppContext.setAttribute(
-        "org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
-        ".*/target/classes/|.*\\.jar");
+        // Look for annotations in the classes directory (dev server) and in the jar
+        // file (live server)
+        webAppContext.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
+                ".*/target/classes/|.*\\.jar");
 
-    // Handle static resources, e.g. html files.
-    ServletHolder defaultServletHolder = webAppContext.addServlet(DefaultServlet.class, "/");
+        // Handle static resources, e.g. html files.
+        final ServletHolder defaultServletHolder = webAppContext.addServlet(DefaultServlet.class, "/");
     defaultServletHolder.setInitParameter("cacheControl", "no-store, max-age=0");
 
     // Start the server! 🚀
