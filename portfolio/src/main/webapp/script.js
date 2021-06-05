31d3f04b9ccaa709c3956cbe030cf8858385a7f7
Ryan Anderson <anderson94@mail.fresnostate.edu>
2021-06-05 08:36:14 +0000

    Add animation

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -43,6 +43,6 @@ $(document).ready(function(){
  $(".userImg").ready(function(){
      $(".userImg").hide(0)
      $(".userImg").show(500)
-     $("#abtMeImg").slide({right:'500px'})
+     $("#abtMeImg").animate({right: '200px'})
  })
 });
