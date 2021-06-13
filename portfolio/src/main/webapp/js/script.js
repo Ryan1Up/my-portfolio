1e39b5dfa67413c45a975255499f219e7b6ce9cc
Ryan Anderson <anderson94@mail.fresnostate.edu>
2021-06-13 06:33:22 +0000

    Testing

---
+++ portfolio/src/main/webapp/js/script.js
@@ -0,0 +1,27 @@
+function loadNav() {
+    
+    $(".navBar").load("../html/nav.html");
+}
+
+function loadFooter() {
+    $(".footer").load("../html/footer.html");
+}
+
+function onLinkLoad(){
+    document.querySelectorAll(".about_me_nav").forEach(item => item.addEventListener("click", event => {
+        event.preventDefault();
+    }, false))
+    
+}
+
+function loadAboutNavMain() {
+    $("#about-content").load("../html/aboutMeContent.html");
+    console.log("About Me");
+}
+function loadAboutNavEducation(event) {
+    console.log("Education");
+}
+function loadAboutNavSkills(event) {
+   
+    $("#about-content").load("../html/languages.html");
+}
