b5542dadba308d3a4b5d1c184319c37a5e1366ff
Ryan Anderson <anderson94@mail.fresnostate.edu>
2021-06-14 06:01:56 +0000

    Add Skill Descriptions

--- portfolio/src/main/webapp/js/script.js
+++ portfolio/src/main/webapp/js/script.js
@@ -2,6 +2,44 @@ function loadNav() {
     
     $(".navbar").load("../nav.html");
 }
+function test() {
+    console.log("Test");
+}
+
+const cDesc =  "<h3>C++</h3><p>I first started to learn to code using C++. I am familiar with most of its basic concepts enough" + 
+    " to do basic data and file processing. Most of my early projects were in C++, and I still" +
+    " plan to use it extensively in the future.</p>"; 
+
+const javaDesc = "<h3>Java</h3><p>Most of my experience in programming has been in Java. I am familiar with the Swing and JavaFx libraries" +
+                  " to help make GUI's. I am planning to use my experience in SPS to elevate my abilities with Java, so that I am " + 
+                  "able to start writing programs the utlize transfer and authentication over the web. I am currently studying concurrency" + 
+                  " in Java in hopes to make my programs more efficient if possible.</p>"; 
+
+const rubyDesc = "<h3>Ruby</h3><p>Funnily enough, my motivations for learning Ruby was purely to fixed a buggy RPG game made with" + 
+                  "RGSS3. My experience with it is very limited to the scope of the Ruby Game Scripting System, but I do have plans" + 
+                  " to learn more about Ruby on Rails as well</p>";
+
+const pythonDesc = "<h3>Python</h3><p>I have learned python in the past, but have rarely used it. Most of my experience around" + 
+                    " python involves debugging other's programs and helping friends add features to their Ren'Py VNs." + 
+                    " I do hope to use it more in the future for other programs.</p>"; 
+
+function loadDesc(arg) {
+    const card = document.getElementById("langDesc");
+    if(arg === 'C++'){
+        card.innerHTML = cDesc;
+    }
+    else if (arg === 'Java'){
+        card.innerHTML = javaDesc;
+    }
+    else if (arg === 'Ruby'){
+        card.innerHTML = rubyDesc;
+    }
+    else if (arg === 'Python'){
+        card.innerHTML = pythonDesc;
+    }
+    
+
+}
 
 function loadFooter() {
     $(".footer").load("../footer.html");
